e8c24c06bb079910ddd1c33b24edf43710fa8ad4
kyleespena <jasyle92@gmail.com>
2023-10-04 00:09:50 +0800

    day 1 completed

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+// DAY 1 Exercise
 // comments can make code readable
 // Welcome to 30DaysOfJavaScript
 /*
    comments can make code readable, easy to reuse and informative
 */
+
+let firstName = "kyle";
+let isMarried = false;
+let middleName;
+let suffix = null;
+
+console.log(
+  typeof firstName,
+  typeof isMarried,
+  typeof middleName,
+  typeof suffix
+);
+let noValue1;
+let noValue2;
+let noValue3;
+let noValue4;
+
+let withValue1 = 1;
+let withValue2 = 2;
+let withValue3 = 3;
+let withValue4 = 4;
+
+// let firstName = "kyle";
+// let lastName = "espena";
+// let isMarried = false;
+// let country = "Philippines";
+
+// let firstName = "kyle",
+//   lastName = "espena",
+//   isMarried = false,
+//   country = "Philippines";
+
+let myAge = 23;
+let yourAge = 24;
+console.log(`
+  I am ${myAge} years old
+  You are ${yourAge} years old
+  `);
